010762923516e5ea316dcc87b7b5680e31f2f9f5
dustbyter <sshah27@ndsj.org>
2026-02-18 17:19:15 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 console.log("I have no clue what any of these words mean anymore. This is pure muscle memory");
 const game = new Phaser.Game({
     type: Phaser.AUTO,
-    width: 1900,
+    width: 1000,
     height: 800,
     backgroundColor: "#339933",
     scale {
-        mode: Phaser.scale.FIT, 
-        autoCenter: Phaser.scale.CENTER_BOTH 
+        mode: Phaser.Scale.FIT, 
+        autoCenter: Phaser.Scale.CENTER_BOTH 
     },  
     scene: {
     }
