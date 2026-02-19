cf1644d799bc2ee901b5066a5a4e515a3aa4505a
dustbyter <sshah27@ndsj.org>
2026-02-18 17:00:16 -0800

    Phaser canvas...?

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("I have no clue what any of these words mean anymore. This is pure muscle memory");
+const game = new Phaser.Game({
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: "#1a1a1a",
+    scene: {
+    }
+});
