f024d49cdf76c1cded58f428e96d46883c546c35
Nahily-Cant-Program <nahilyyadeta@gmail.com>
2026-03-11 22:13:45 -0700

    Update script.js

--- script.js
+++ script.js
@@ -175,7 +175,7 @@ if (document.getElementById("question")) {
             potionName = potionNames[q.reward];
             result(randMess1 + potionName);
           }, 1000);
-           counter.innerHTML + 1;
+           counting.innerHTML + 1;
         }
         else {
           loseLives();
